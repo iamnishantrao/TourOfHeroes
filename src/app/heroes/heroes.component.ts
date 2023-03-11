ec1c74932fdcaf61f717ccc0ed5bc24a69fab77f
Nishant Yadav <nishantyadav025@gmail.com>
2023-03-11 02:00:42 -0800

    message service and component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from "../mock-heroes";
 import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -12,7 +12,7 @@ export class HeroesComponent {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
   ngOnInit(): void {
@@ -26,5 +26,5 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  }
+    this.messageService.add(`HeroesComponent: Selected Hero Id = ${hero.id}`);  }
 }
